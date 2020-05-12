c5a018264371130b78e489471aa1e29d515a3e0a
tolochenkovad <vadim.tolochenko@gmail.com>
2020-05-12 21:57:10 +0300

    Updates

---
+++ precache-manifest.e3a187641c150ab386b619a9e01fe657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d760f9ec2ab42fd093b0ef79f292632",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "4a2d42855ddce24d2f43",
+    "url": "/notes/static/css/2.45780812.chunk.css"
+  },
+  {
+    "revision": "e82ac1fabdaa7f2da749",
+    "url": "/notes/static/css/main.54a352c0.chunk.css"
+  },
+  {
+    "revision": "4a2d42855ddce24d2f43",
+    "url": "/notes/static/js/2.d708a9ca.chunk.js"
+  },
+  {
+    "revision": "77d9a5a8d0635e146e324553e360082f",
+    "url": "/notes/static/js/2.d708a9ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e82ac1fabdaa7f2da749",
+    "url": "/notes/static/js/main.d16fed9b.chunk.js"
+  },
+  {
+    "revision": "9479608c89c7f2dbb78e",
+    "url": "/notes/static/js/runtime-main.f40c402c.js"
+  }
+]);
